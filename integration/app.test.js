e139a10393e8177c9cd9ed65a390aee5f501c2dc
Dergacz <sats96dergach@gmail.com>
2021-08-19 12:15:19 +0300

    add snapshot test for other commponents

--- integration/app.test.js
+++ integration/app.test.js
@@ -8,3 +8,37 @@ describe('addItemForm', () => {
         expect(image).toMatchImageSnapshot();
     });
 });
+
+describe('editableSpan', () => {
+    it('base example, visually looks correct editableSpan', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-editablespan--editable-span-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('appWithRedux', () => {
+    it('base example, visually looks correct appWithRedux', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-appwithredux--app-with-redux-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('task', () => {
+    it('base example, visually looks correct task', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-task--task-is-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
